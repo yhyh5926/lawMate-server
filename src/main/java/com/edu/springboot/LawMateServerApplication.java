5683636c3e04955c3a533fdb5e521d3ddd6f9596
shark04-cmd <tkddjejrgn04@gmail.com>
2026-02-25 16:19:11 +0900

    푸쉬

--- src/main/java/com/edu/springboot/LawMateServerApplication.java
+++ src/main/java/com/edu/springboot/LawMateServerApplication.java
@@ -1,9 +1,11 @@
 package com.edu.springboot;
 
+import org.mybatis.spring.annotation.MapperScan; // 1. 이 줄 추가
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.edu.springboot.domain") // 2. 이 줄 추가 (domain 폴더 안의 모든 매퍼 검색)
 public class LawMateServerApplication {
 
 	public static void main(String[] args) {
